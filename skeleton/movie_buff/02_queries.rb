5bc64c4ba8bcbd063859cf00a6c5e5c9581a0d7e
nilshen <linshen88@gmail.com>
2022-04-29 15:56:02 -0400

    w5d5_eod

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -2,19 +2,42 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989)
+    .where(score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
+  Movie
+    .select(:yr).distinct
+    .group(:yr)
+    .having('MAX(movies.score) <= 8')
+    .pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  # Actor
+  #   .select(:id, :name)
+  #   .joins(:movies)
+  #   .where('movies.title IN (?)', title )
+  #   .order('castings.ord ASC')
+  
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where(movies: {title: title} )
+    .order('castings.ord ASC')
 
 end
+
+
+
+
 
 def vanity_projects
   # List the title of all movies in which the director also appeared
