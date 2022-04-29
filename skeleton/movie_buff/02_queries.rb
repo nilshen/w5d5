383f162c2e563614a8156a4c8abbc7dca11185b2
nilshen <linshen88@gmail.com>
2022-04-29 16:10:26 -0400

    w5d5_EOD

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -2,10 +2,14 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
+  # Movie
+  #   .select(:id, :title, :yr, :score)
+  #   .where(yr: 1980..1989)
+  #   .where(score: 3..5)
   Movie
     .select(:id, :title, :yr, :score)
-    .where(yr: 1980..1989)
-    .where(score: 3..5)
+    .where('yr BETWEEN 1980 AND 1989')
+    .where('score BETWEEN 3 AND 5')
 end
 
 def bad_years
